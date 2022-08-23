f995572efb9fb94fa735cf69e322cde208a33519
Daria Strebkova <daria.strebkova@mail.com>
2022-08-23 22:43:08 +0300

    add new code

--- index.js
+++ index.js
@@ -96,6 +96,20 @@ $(".interactive-avatar__link").click(e => {
 
 ///form///
 
+const validateFields = (form, fieldsArray) => {
+
+  fieldsArray.forEach(field => {
+    field.removeClass("input-error");
+    if (field.val().trim() === "") {
+      field.addClass("input-error");
+    }
+  })
+
+  const errorFields = form.find(".input-error");
+
+  return errorFields.length === 0;
+}
+
 $(".form").submit((e) => {
   e.preventDefault();
 
@@ -105,24 +119,46 @@ $(".form").submit((e) => {
   const comment = form.find("[name='comment']");
   const to = form.find("[name='to']");
 
-  $.ajax({
-    url: "https://webdev-api.loftschool.com/sendmail",
-    method: "post",
-    data: {
-      name: name.val(),
-      phone: phone.val(),
-      comment: comment.val(),
-      to: to.val(),
-    }
-  });
+  const modal = $("#modal");
+  const content = modal.find(".modal__content");
 
-   $.fancybox.open({
-     src: "#modal",
-     type: "inline"
-   })
+  modal.removeClass("error-modal");
+
+  const isValid = validateFields(form, [name, phone, comment, to]);
+
+  if (isValid) {
+    const request = $.ajax({
+      url: "https://webdev-api.loftschool.com/sendmail",
+      method: "post",
+      data: {
+        name: name.val(),
+        phone: phone.val(),
+        comment: comment.val(),
+        to: to.val(),
+      },
+      error: (data) => {},
+    });
+
+    request.done((data) => {
+      content.text(data.message);
+    })
+
+    request.fail((data) => {
+      const message = data.responseJSON.message;
+      content.text(message);
+      modal.addClass("error-modal");     
+    })
+
+    request.always(() => {
+      $.fancybox.open({
+        src: "#modal",
+        type: "inline",
+      });
+    })
+  }  
 });
 
-$(".app-close-modal").click(e => {
+$(".app-close-modal").click((e) => {
   e.preventDefault();
 
   $.fancybox.close();
